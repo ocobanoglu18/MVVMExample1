0fd9baa2d02c3c51488d2db51612db42921cafb4
ocobanoglu18 <ocobanoglu18@ku.edu.tr>
2022-11-15 11:25:28 +0300

    Initial Commit

---
+++ MVVM Example/MVVM_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_ExampleApp.swift
+//  MVVM Example
+//
+//  Created by Adnan Cobanoglu on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
